6471bd46a3a7eabd8cb19a281abd0cd5bc5e8980
unknown <tarkowr@gmail.com>
2019-05-31 21:31:28 -0400

    Updated button styles

--- StockInsight/MainWindow.xaml.cs
+++ StockInsight/MainWindow.xaml.cs
@@ -62,6 +62,10 @@ namespace StockInsight
                     }
                 }
             }
+            else
+            {
+                errorOnLoad = true;
+            }
         }
 
         /// <summary>
